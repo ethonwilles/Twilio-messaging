7dc02b695d011d2b695322e642ab24e0b9429e0c
ethonwilles <ethonwilles@gmail.com>
2019-10-02 09:43:52 -0600

    Added Database Functionality

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask, request, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 import config
-from flask_pymongo import PyMongo
+import pymongo
 app = Flask(__name__)
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient['twilioDatabase']
+
+mycol = mydb["users"]
+
 
 account_sid = config.ACCOUNT_SID
 auth_token = config.AUTH_TOKEN
@@ -21,10 +27,15 @@ for contact in contacts:
     list_of_contacts.append(contact)
 print(list_of_contacts)
 
+
+
+# index page routes and logic
 @app.route('/', methods=['GET'])
 def render():
     return render_template('index.html')
 
+
+# submission page routes and logic
 @app.route('/submission.html', methods=['GET'])
 def render_sub():
     
@@ -32,6 +43,27 @@ def render_sub():
     message = request.form['message']
     print(message)
 
+# Conversation page routes and logic
+@app.route('/conversations.html', methods=["GET"])
+def render_conv():
+    return render_template('conversations.html')
+
+
+# Sign up Page routes and logic
+@app.route('/signup.html', methods=["GET"])
+def render_signup():
+    return render_template('signup.html')
+
+@app.route('/signup.html', methods=['POST'])
+def add_to_database():
+
+    new_dict = {'username': f'{request.form["username"]}', 'password' : f'{request.form["password"]}'}
+    mycol.insert_one(new_dict)
+    print(mycol.find_one())
+    return render_template('/conversations.html')
+
+
+# send sms?
 @app.route('/send_sms', methods=['VIEW'])
 def send_sms():
     message = client.messages \
@@ -54,12 +86,14 @@ def sms_response():
     return str(resp)
 
 
-# Mongo DB
-app.config['MONGO_URI'] = "mongodb://localhost:27017/twilioDatabase"
-mongo = PyMongo(app)
+
+
 
 
 
 
 if __name__ == '__main__':
     app.run(port=80,debug=True)
+
+
+print(mycol.fine_one())
